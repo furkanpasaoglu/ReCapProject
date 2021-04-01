61b8f0798795d10939971234c116528baa256f24
furkan paşaoğlu <furkan.pasaoglu@hotmail.com>
2021-04-01 17:18:48 +0300

    Update

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -57,7 +57,6 @@ namespace Business.Concrete
             return new SuccessResult(Messages.UserDeleted);
         }
 
-        [CacheAspect]
         public IDataResult<List<User>> GetAll()
         {
             if (DateTime.Now.Hour == 00)
@@ -67,7 +66,6 @@ namespace Business.Concrete
             return new SuccessDataResult<List<User>>(_userDal.GetAll(), Messages.UserListed);
         }
 
-        [CacheAspect]
         [PerformanceAspect(5)]
         public IDataResult<User> GetById(int id)
         {
@@ -96,13 +94,11 @@ namespace Business.Concrete
             return new SuccessResult(Messages.UserUpdated);
         }
 
-        [CacheAspect]
         public IDataResult<List<OperationClaim>> GetClaims(User user)
         {
             return new SuccessDataResult<List<OperationClaim>>(_userDal.GetClaims(user));
         }
 
-        [CacheAspect]
         [PerformanceAspect(5)]
         public IDataResult<User> GetByMail(string email)
         {
